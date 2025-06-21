2f6096e02309f58516a8ea2921698b9865fa8ef2
Andres Correa <felipefranco989@gmail.com>
2025-06-21 09:10:53 -0500

    Se agrega archvio feature y stepdefinition

---
+++ src/test/java/com/co/starter/stepdefinitions/CreateAPersonTypeCustomer.java
@@ -0,0 +1,27 @@
+package com.co.starter.stepdefinitions;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+
+public class CreateAPersonTypeCustomer {
+    
+    @Dado("que el usuario esta en la pagina crear un tercero")
+    public void queElUsuarioEstaEnLaPaginaCrearUnTercero() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Cuando("el usuario completa el formulario con los datos del cliente y guarda")
+    public void elUsuarioCompletaElFormularioConLosDatosDelClienteYGuarda() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Entonces("debe ser redirigido a una pagina con un que coniene el nombre del cliente y estado activo")
+    public void debeSerRedirigidoAUnaPaginaConUnQueConieneElNombreDelClienteYEstadoActivo() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
